16d698b51330ad5d4bf8e6895c93db1d4596c7aa
Max Yang <max.yang342@gmail.com>
2024-10-09 15:11:34 -0400

    fix cosine law and implement asymptote

--- Static/script.js
+++ Static/script.js
@@ -1,13 +1,58 @@
 function CosineLaw(aSide, bSide, cAngle) {
-    return Math.sqrt(Math.pow(aSide, 2) + Math.pow(bSide, 2) - (2 * aSide * bSide) * Math.cos(cAngle))
+    let cAngleRadians = cAngle * (Math.PI / 180);
+    return Math.sqrt(Math.pow(aSide, 2) + Math.pow(bSide, 2) - (2 * aSide * bSide) * Math.cos(cAngleRadians))
 }
 
-const cosineButton = document.querySelector(".cosine-calculate");
-let cosineLawOutput = document.querySelector(".cosine-side-c");
+function Asymptote(mValue, nValue) {
+    if (mValue == nValue) {
+        return "The asymptote is horizontal";
+    }
+    else if (nValue > mValue) {
+        return "The asymptote is the x-axis";
+    }
+    else {
+        switch(mValue - nValue) {
+            case 1:
+                return "Linear";
+            case 2:
+                return "Quadratic";
+            case 3:
+                return "Cubic";
+            case 4:
+                return "Quartic";
+            case 5:
+                return "Quintic";
+            case 6:
+                return "Sextic";
+            case 7:
+                return "Septic";
+            case 8:
+                return "Octic";
+            case 9:
+                return "Nonic";
+            case 10:
+                return "Decic";
+        }
+    }
+}
 
-cosineButton.addEventListener("click", function() {
-    let aSide = document.getElementById("cosine-side-a");
-    let bSide = document.getElementById("cosine-side-b");
-    let cAngle = document.getElementById("cosine-angle-c");
+function CalculateCosineLaw(e) {
+    e.preventDefault();
+
+    let aSide = parseFloat(document.getElementById("cosine-side-a").value);
+    let bSide = parseFloat(document.getElementById("cosine-side-b").value);
+    let cAngle = parseFloat(document.getElementById("cosine-angle-c").value);
+    const cosineLawOutput = document.querySelector(".cosine-side-c");
+
     cosineLawOutput.value = CosineLaw(aSide, bSide, cAngle);
-})
+}
+
+function CalculateAsymptote(e) {
+    e.preventDefault();
+
+    let nValue = parseFloat(document.getElementById("asymptote-n-value").value);
+    let mValue = parseFloat(document.getElementById("asymptote-m-value").value);
+    const asymptoteTypeOutput = document.querySelector(".asymptote-type");
+
+    asymptoteTypeOutput.value = Asymptote(mValue, nValue);
+}
